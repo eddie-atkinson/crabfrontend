2a3824aaab7b080a819835ab9f390ef0c33330f5
Eddie Atkinson <>
2021-03-21 14:33:16 +0800

    fixes background image a bit

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
 
 <style>
 body {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url("/Crab2.svg");
+  width: 100%;
   background-size: cover;
-  background-attachment: fixed;
+  height: 0;
+  padding: 0; /* reset */
+  padding-bottom: calc(100% * 3 / 4);
 }
 </style>
